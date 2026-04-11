106f83869ade9e024b505875787362bb5e21484c
SundownerSport <kilchisblue@aol.com>
2026-04-11 13:33:58 -0700

    Re-enable Vulkan — all deps now available

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ cmake ../yabause \
     -DYAB_PORTS=retro_arena \
     -DYAB_WANT_OPENGL=ON \
     -DYAB_WANT_SDL=ON \
-    -DYAB_WANT_VULKAN=OFF \
+    -DYAB_WANT_VULKAN=ON \
     -DYAB_WANT_DYNAREC_DEVMIYAX=ON \
     -DYAB_MULTIBUILD=OFF
 make -j$(nproc)
